66a4e67d182748bd57e6de34bcf32768c6731476
Kiet Tuan <101494254+ktcshust@users.noreply.github.com>
2025-08-04 15:11:21 +0700

    Create customer.py

---
+++ customer.py
@@ -0,0 +1,9 @@
+class Customer:
+    def __init__(self, customer_id, name, email):
+        self.customer_id = customer_id
+        self.name = name
+        self.email = email
+        self.bookings = []
+
+    def add_booking(self, booking):
+        self.bookings.append(booking)
