98257fbf9251d9d0032d6873c022be2257a3598d
Kiet Tuan <101494254+ktcshust@users.noreply.github.com>
2025-08-04 15:53:49 +0700

    Update room.py

--- room.py
+++ room.py
@@ -4,6 +4,15 @@ class Room:
         self.type = type
         self.price = price
         self.is_available = True
+        self.reviews = []
 
     def set_availability(self, is_available):
         self.is_available = is_available
+
+    def add_review(self, customer, rating, comment):
+        self.reviews.append({
+            "customer": customer.name,
+            "rating": rating,
+            "comment": comment
+        })
+        return f"Review added by {customer.name}"
